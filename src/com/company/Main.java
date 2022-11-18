fc1f9ccb58cfd0a0aaab6967e4d86ebcfa6c289a
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 17:44:40 +0530

    ADDED: Code to check for one of thr three options(noplay,ladder,snake)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,26 @@
 package com.company;
 
 public class Main {
+    static final int NO_PLAY = 1;
+    static final int LADDER = 2;
+    static final int SNAKE = 3;
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Snake And Ladder Game");
-        int startPosition=0;
-        int dice=(int)Math.floor((Math.random()*10)%6)+1;
+        int startPosition = 0;
+        int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
+        int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
+        switch (option) {
+            case NO_PLAY:
+                break;
+            case LADDER:
+                startPosition += dice;
+                break;
+            case SNAKE:
+                startPosition -= startPosition;
+                startPosition = startPosition < 0 ? 0 : startPosition;
+                break;
+        }
     }
 }
