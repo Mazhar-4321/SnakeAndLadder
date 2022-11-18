851db70221d68ce9fae9e987d456456f8ef15c86
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 17:29:28 +0530

    ADDED: Code to start position at 0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Snake And Ladder Game");
+        int startPosition=0;
     }
 }
