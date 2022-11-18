0b3f5916ae501468741aaee0e9e9b0a608a65e27
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 19:31:20 +0530

    ADDED: Code to allow a play till startposition of 100 is reached

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,26 @@ public class Main {
     static final int NO_PLAY = 1;
     static final int LADDER = 2;
     static final int SNAKE = 3;
+    static final int WINNING_POSITION = 100;
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("Snake And Ladder Game");
         int startPosition = 0;
-        int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
-        int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
-        switch (option) {
-            case NO_PLAY:
-                break;
-            case LADDER:
-                startPosition += dice;
-                break;
-            case SNAKE:
-                startPosition -= startPosition;
-                startPosition = startPosition < 0 ? 0 : startPosition;
-                break;
+        while (startPosition <= 100) {
+            int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
+            int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
+            switch (option) {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    startPosition += dice;
+                    break;
+                case SNAKE:
+                    startPosition -= startPosition;
+                    startPosition = startPosition < 0 ? 0 : startPosition;
+                    break;
+            }
         }
     }
 }
