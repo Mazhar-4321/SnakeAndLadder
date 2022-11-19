61a06f12e64c246fae2838e02c5998aab513167c
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 07:09:06 +0530

    MODIFIED: Code to remove unwanted constants and cases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,24 @@
 package com.company;
 
 public class Main {
-    static final int NO_PLAY = 1;
     static final int LADDER = 2;
     static final int SNAKE = 3;
     static final int WINNING_POSITION = 100;
+    static final int INITIAL_POSITION = 0;
 
     public static void main(String[] args) {
         System.out.println("Snake And Ladder Game");
-        int startPosition = 0;
-        while (startPosition <= WINNING_POSITION) {
+        int startPosition = INITIAL_POSITION;
+        while (startPosition != WINNING_POSITION) {
             int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
             int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
             switch (option) {
-                case NO_PLAY:
-                    break;
                 case LADDER:
                     startPosition = startPosition + dice > WINNING_POSITION ? startPosition : startPosition + dice;
                     break;
                 case SNAKE:
-                    startPosition -= startPosition;
-                    startPosition = startPosition < 0 ? 0 : startPosition;
+                    startPosition -= dice;
+                    startPosition = startPosition < INITIAL_POSITION ? INITIAL_POSITION : startPosition;
                     break;
             }
         }
