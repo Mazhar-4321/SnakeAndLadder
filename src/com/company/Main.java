2c07ddccd90d326eeb4be351a71ca2c3f65ea157
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 09:06:37 +0530

    ADDED: Code to enable game between two players

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,33 +3,65 @@ package com.company;
 import java.util.*;
 
 public class Main {
+    static final int NO_PLAY=1;
     static final int LADDER = 2;
     static final int SNAKE = 3;
     static final int WINNING_POSITION = 100;
     static final int INITIAL_POSITION = 0;
+    static final int PLAYER_1 = 1;
+    static final int PLAYER_2 = 2;
 
     public static void main(String[] args) {
         System.out.println("Snake And Ladder Game");
         int startPosition = INITIAL_POSITION;
-        int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
-        ArrayList<Integer> positionList = new ArrayList<>();
+        ArrayList<Integer>> player1List = new ArrayList<>();
+        ArrayList<Integer>> player2List = new ArrayList<>();
         int diceCount = 0;
-        while (startPosition != WINNING_POSITION) {
+        int player1CurrentScore = INITIAL_POSITION;
+        int player2CurrentScore = INITIAL_POSITION;
+        int currentPlayer = PLAYER_1;
+        while (player1CurrentScore!=WINNING_POSITION || player2CurrentScore!=WINNING_POSITION) {
             int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
             int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
             switch (option) {
+                case NO_PLAY:
+                    if(currentPlayer==PLAYER_1) {
+                        currentPlayer=PLAYER_2;
+                          }else {
+                        currentPlayer=PLAYER_1;
+                        }
+                    break;
                 case LADDER:
-                    startPosition = startPosition + dice > WINNING_POSITION ? startPosition : startPosition + dice;
+                    if(currentPlayer==PLAYER_1) {
+                        player1CurrentScore = player1CurrentScore + dice > WINNING_POSITION ? player1CurrentScore : player1CurrentScore + dice;
+                        player1CurrentScore.add(player1CurrentScore);
+                    }else {
+                        player2CurrentScore = player2CurrentScore + dice > WINNING_POSITION ? player2CurrentScore : player2CurrentScore + dice;
+                        player2CurrentScore.add(player2CurrentScore);
+                    }
                     break;
                 case SNAKE:
-                    startPosition -= dice;
-                    startPosition = startPosition < INITIAL_POSITION ? INITIAL_POSITION : startPosition;
+                    if(currentPlayer==PLAYER_1) {
+                        player1CurrentScore-=dice;
+                        player1CurrentScore = player1CurrentScore < INITIAL_POSITION ? INITIAL_POSITION : player1CurrentScore;
+                        currentPlayer=PLAYER_2;
+                        player1CurrentScore.add(player1CurrentScore);
+                    }else {
+                        player2CurrentScore-=dice;
+                        player2CurrentScore = player2CurrentScore < INITIAL_POSITION ? INITIAL_POSITION : player2CurrentScore;
+                        currentPlayer=PLAYER_1;
+                        player2CurrentScore.add(player2CurrentScore);
+                    }
                     break;
             }
             positionList.add(startPosition);
             diceCount++;
         }
-        System.out.println("Dice Count="+diceCount+","+"Position List="+positionList);
-    }
+        System.out.println("Dice Count=" + diceCount + "," + "Position List=" + positionList);
+        if(player1CurrentScore==WINNING_POSITION){
+            System.out.println("Player1 Won , Positions="+player1List);
+    }else{
+            System.out.println("Player2 Won, Positions="+player2List);
+        }
 }
 }
