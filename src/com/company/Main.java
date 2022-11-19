28785685ac717548c5a9ac6d3904b35ab7fb1437
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 06:40:32 +0530

    MODIFIED: Code to remove comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,16 @@ public class Main {
     static final int WINNING_POSITION = 100;
 
     public static void main(String[] args) {
-        // write your code here
         System.out.println("Snake And Ladder Game");
         int startPosition = 0;
-        while (startPosition <= 100) {
+        while (startPosition <= WINNING_POSITION) {
             int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
             int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
             switch (option) {
                 case NO_PLAY:
                     break;
                 case LADDER:
-                    startPosition = startPosition + dice > 100 ? startPosition : startPosition + dice;
+                    startPosition = startPosition + dice > WINNING_POSITION ? startPosition : startPosition + dice;
                     break;
                 case SNAKE:
                     startPosition -= startPosition;
