e4fa9d4d999d4632aa883058a5e8e87496c5a368
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 06:32:38 +0530

    MODIFIED: Code to give spacing and remove unwanted comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Snake And Ladder Game");
-        int startPosition=0;
+        int startPosition = 0;
     }
 }
