2d56d3b22795b0c53df32e02c9f73284145ac00f
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 09:08:08 +0530

    MODIFIED: Code to add indentation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.*;
 
 public class Main {
-    static final int NO_PLAY=1;
+    static final int NO_PLAY = 1;
     static final int LADDER = 2;
     static final int SNAKE = 3;
     static final int WINNING_POSITION = 100;
@@ -14,42 +14,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Snake And Ladder Game");
         int startPosition = INITIAL_POSITION;
-        ArrayList<Integer>> player1List = new ArrayList<>();
-        ArrayList<Integer>> player2List = new ArrayList<>();
+        ArrayList<Integer>>player1List = new ArrayList<>();
+        ArrayList<Integer>>player2List = new ArrayList<>();
         int diceCount = 0;
         int player1CurrentScore = INITIAL_POSITION;
         int player2CurrentScore = INITIAL_POSITION;
         int currentPlayer = PLAYER_1;
-        while (player1CurrentScore!=WINNING_POSITION || player2CurrentScore!=WINNING_POSITION) {
+        while (player1CurrentScore != WINNING_POSITION || player2CurrentScore != WINNING_POSITION) {
             int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
             int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
             switch (option) {
                 case NO_PLAY:
-                    if(currentPlayer==PLAYER_1) {
-                        currentPlayer=PLAYER_2;
-                          }else {
-                        currentPlayer=PLAYER_1;
-                        }
+                    if (currentPlayer == PLAYER_1) {
+                        currentPlayer = PLAYER_2;
+                    } else {
+                        currentPlayer = PLAYER_1;
+                    }
                     break;
                 case LADDER:
-                    if(currentPlayer==PLAYER_1) {
+                    if (currentPlayer == PLAYER_1) {
                         player1CurrentScore = player1CurrentScore + dice > WINNING_POSITION ? player1CurrentScore : player1CurrentScore + dice;
                         player1CurrentScore.add(player1CurrentScore);
-                    }else {
+                    } else {
                         player2CurrentScore = player2CurrentScore + dice > WINNING_POSITION ? player2CurrentScore : player2CurrentScore + dice;
                         player2CurrentScore.add(player2CurrentScore);
                     }
                     break;
                 case SNAKE:
-                    if(currentPlayer==PLAYER_1) {
-                        player1CurrentScore-=dice;
+                    if (currentPlayer == PLAYER_1) {
+                        player1CurrentScore -= dice;
                         player1CurrentScore = player1CurrentScore < INITIAL_POSITION ? INITIAL_POSITION : player1CurrentScore;
-                        currentPlayer=PLAYER_2;
+                        currentPlayer = PLAYER_2;
                         player1CurrentScore.add(player1CurrentScore);
-                    }else {
-                        player2CurrentScore-=dice;
+                    } else {
+                        player2CurrentScore -= dice;
                         player2CurrentScore = player2CurrentScore < INITIAL_POSITION ? INITIAL_POSITION : player2CurrentScore;
-                        currentPlayer=PLAYER_1;
+                        currentPlayer = PLAYER_1;
                         player2CurrentScore.add(player2CurrentScore);
                     }
                     break;
@@ -58,10 +58,10 @@ public class Main {
             diceCount++;
         }
         System.out.println("Dice Count=" + diceCount + "," + "Position List=" + positionList);
-        if(player1CurrentScore==WINNING_POSITION){
-            System.out.println("Player1 Won , Positions="+player1List);
-    }else{
-            System.out.println("Player2 Won, Positions="+player2List);
+        if (player1CurrentScore == WINNING_POSITION) {
+            System.out.println("Player1 Won , Positions=" + player1List);
+        } else {
+            System.out.println("Player2 Won, Positions=" + player2List);
         }
-}
+    }
 }
