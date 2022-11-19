75190ca82e45b523b4cd5bce3bc21e59548c29cd
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 06:36:21 +0530

    MODIFIED: Code to remove comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
     static final int SNAKE = 3;
 
     public static void main(String[] args) {
-        // write your code here
         System.out.println("Snake And Ladder Game");
         int startPosition = 0;
         int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
