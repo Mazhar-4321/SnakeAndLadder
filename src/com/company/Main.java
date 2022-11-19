fa8306a729b61e97d3554c5b45457fad7291241f
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 06:52:06 +0530

    MODIFIED: Code to remove constants and cases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 public class Main {
-    static final int NO_PLAY = 1;
     static final int LADDER = 2;
     static final int SNAKE = 3;
     static final int WINNING_POSITION = 100;
@@ -13,8 +12,6 @@ public class Main {
             int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
             int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
             switch (option) {
-                case NO_PLAY:
-                    break;
                 case LADDER:
                     startPosition = startPosition + dice > WINNING_POSITION ? startPosition : startPosition + dice;
                     break;
