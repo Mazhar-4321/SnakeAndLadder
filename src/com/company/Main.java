baedc12d58ae3e9c17c4c3ddfa92996161c0c194
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 07:01:43 +0530

    MODIFIED: Code to replace constant values with constant variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,11 @@ public class Main {
     static final int NO_PLAY = 1;
     static final int LADDER = 2;
     static final int SNAKE = 3;
+    static final int INITIAL_POSITION = 0;
 
     public static void main(String[] args) {
         System.out.println("Snake And Ladder Game");
-        int startPosition = 0;
+        int startPosition = INITIAL_POSITION;
         int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
         int option = (int) Math.floor((Math.random() * 10) % 3) + 1;
         switch (option) {
@@ -18,7 +19,7 @@ public class Main {
                 break;
             case SNAKE:
                 startPosition -= startPosition;
-                startPosition = startPosition < 0 ? 0 : startPosition;
+                startPosition = startPosition < INITIAL_POSITION ? INITIAL_POSITION : startPosition;
                 break;
         }
     }
